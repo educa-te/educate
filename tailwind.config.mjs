a5554e52f1102319ed6782caeedaeca397274475
Educa-te <educa.te.ebook@gmail.com>
2024-09-13 16:51:01 -0300

    home v1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {keyframes: {
+			fadeInOut: {
+			  '0%, 100%': { opacity: 0 }, // Empieza y termina invisible
+			  '50%': { opacity: 1 },       // En el medio es visible
+			},
+		  },
+		  animation: {
+			fadeInOut: 'fadeInOut 4s ease-in-out infinite', // Duración 4s, animación infinita
+		  },},
 	},
 	plugins: [],
 }
